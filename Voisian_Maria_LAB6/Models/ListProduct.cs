1c593a28268f0961ef3ccf8e384e70e62033777f
MariaVoisian <91866647+MariaVoisian@users.noreply.github.com>
2022-01-03 10:03:12 +0200

    Add project files.

---
+++ Voisian_Maria_LAB6/Models/ListProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+namespace Voisian_Maria_LAB6.Models
+{
+    public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ Voisian_Maria_LAB6/Models/ShopList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voisian_Maria_LAB6.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
